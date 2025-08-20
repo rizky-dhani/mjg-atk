3a404c1eb27f6ee064fe83dcaefc05e771aecbb4
Rizky Dhani <rizkydhni03@gmail.com>
2025-08-20 09:51:14 +0700

    update models name

--- database/seeders/MarketingMediaSeeder.php
+++ database/seeders/MarketingMediaSeeder.php
@@ -3,8 +3,9 @@
 namespace Database\Seeders;
 
 use App\Models\MarketingMedia;
-use App\Models\MarketingMediaCategory;
 use Illuminate\Database\Seeder;
+use App\Models\MarketingMediaItem;
+use App\Models\MarketingMediaCategory;
 
 class MarketingMediaSeeder extends Seeder
 {
@@ -20,7 +21,7 @@ class MarketingMediaSeeder extends Seeder
         }
 
         foreach ($categories as $category) {
-            MarketingMedia::create([
+            MarketingMediaItem::create([
                 'name' => $category->name . ' Media',
                 'slug' => $category->slug . '-media',
                 'category_id' => $category->id,
