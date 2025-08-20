75cf2c39e007d94e46e59c9dd36ce4803e836146
Rizky Dhani <rizkydhni03@gmail.com>
2025-08-20 14:08:05 +0700

    divide Office Stationery Stock Request and Stock Usage for Div Admin

--- app/Filament/Widgets/DivisionOfficeStationeryStockRequestStatus.php
+++ app/Filament/Widgets/DivisionOfficeStationeryStockRequestStatus.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\OfficeStationeryStockRequest;
+use App\Models\OfficeStationeryStockUsage;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
 use Filament\Widgets\StatsOverviewWidget\Stat;
 use Illuminate\Support\Facades\Auth;
@@ -10,10 +11,10 @@ use Illuminate\Support\Facades\Auth;
 class DivisionOfficeStationeryStockRequestStatus extends BaseWidget
 {
     protected static bool $isLazy = false;
-    protected ?string $heading = 'Office Stationery Stock Request';
+    protected ?string $heading = 'Office Stationery';
     protected function getColumns(): int
     {
-        return 3;
+        return 4;
     }
     protected static ?int $sort = 1;
     protected function getStats(): array
@@ -21,22 +22,40 @@ class DivisionOfficeStationeryStockRequestStatus extends BaseWidget
         $user = Auth::user();
         $divisionId = $user->division_id;
         
-        // Get counts for all statuses
-        $pendingCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
+        // Get counts for all Stock Request statuses
+        $pendingStockRequestCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
             ->where('status', OfficeStationeryStockRequest::STATUS_PENDING)
             ->count();
-        $inProgressCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
-            ->where('status', '!=' , [OfficeStationeryStockRequest::STATUS_PENDING, OfficeStationeryStockRequest::STATUS_COMPLETED])
+        $inProgressStockRequestCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
+            ->whereNotIn('status', [OfficeStationeryStockRequest::STATUS_PENDING, OfficeStationeryStockRequest::STATUS_REJECTED_BY_HEAD, OfficeStationeryStockRequest::STATUS_REJECTED_BY_IPC, OfficeStationeryStockRequest::STATUS_REJECTED_BY_SECOND_IPC_HEAD, OfficeStationeryStockRequest::STATUS_REJECTED_BY_GA_ADMIN, OfficeStationeryStockRequest::STATUS_REJECTED_BY_HCG_HEAD, OfficeStationeryStockRequest::STATUS_COMPLETED])
             ->count();
-        $completedCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
+        $rejectedStockRequestCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
+            ->whereIn('status', [OfficeStationeryStockRequest::STATUS_REJECTED_BY_HEAD, OfficeStationeryStockRequest::STATUS_REJECTED_BY_IPC, OfficeStationeryStockRequest::STATUS_REJECTED_BY_IPC_HEAD, OfficeStationeryStockRequest::STATUS_REJECTED_BY_SECOND_IPC_HEAD, OfficeStationeryStockRequest::STATUS_REJECTED_BY_GA_ADMIN, OfficeStationeryStockRequest::STATUS_REJECTED_BY_HCG_HEAD])
+            ->count();
+        $completedStockRequestCount = OfficeStationeryStockRequest::where('division_id', $divisionId)
             ->where('status', OfficeStationeryStockRequest::STATUS_COMPLETED)
             ->count();
 
+        // Get counts for all Stock Usage statuses
+        $pendingStockUsageCount = OfficeStationeryStockUsage::where('division_id', $divisionId)
+            ->where('status', OfficeStationeryStockUsage::STATUS_PENDING)
+            ->count();
+        $inProgressStockUsageCount = OfficeStationeryStockUsage::where('division_id', $divisionId)
+            ->whereNotIn('status', [OfficeStationeryStockUsage::STATUS_REJECTED_BY_HEAD, OfficeStationeryStockUsage::STATUS_REJECTED_BY_GA_ADMIN, OfficeStationeryStockUsage::STATUS_REJECTED_BY_HCG_HEAD])
+            ->count();
+        $rejectedStockUsageCount = OfficeStationeryStockUsage::where('division_id', $divisionId)
+            ->whereIn('status', [OfficeStationeryStockUsage::STATUS_REJECTED_BY_HEAD, OfficeStationeryStockUsage::STATUS_REJECTED_BY_GA_ADMIN, OfficeStationeryStockUsage::STATUS_REJECTED_BY_HCG_HEAD])
+            ->count();
+        $completedStockUsageCount = OfficeStationeryStockUsage::where('division_id', $divisionId)
+            ->where('status', OfficeStationeryStockUsage::STATUS_COMPLETED)
+            ->count();
+
         return [
-            Stat::make('Pending', $pendingCount)
-                ->description('Pending approval from head of division')
+            // Stock Requests
+            Stat::make('Stock Request', $pendingStockRequestCount)
+                ->description('Pending approval')
                 ->descriptionIcon('heroicon-m-clock')
-                ->color('danger')
+                ->color('primary')
                 ->url(
                     route('filament.dashboard.resources.office-stationery-stock-requests.index', [
                         'tableFilters[status][value]' => OfficeStationeryStockRequest::STATUS_PENDING
@@ -44,7 +63,7 @@ class DivisionOfficeStationeryStockRequestStatus extends BaseWidget
                 )
                 ->icon('heroicon-o-document-text'),
 
-            Stat::make('In Progress', $inProgressCount)
+            Stat::make('Stock Request', $inProgressStockRequestCount)
                 ->description('In progress')
                 ->descriptionIcon('heroicon-m-clock')
                 ->color('warning')
@@ -53,13 +72,63 @@ class DivisionOfficeStationeryStockRequestStatus extends BaseWidget
                 )
                 ->icon('heroicon-o-document-text'),
             
-            Stat::make('Completed', $completedCount)
+            Stat::make('Stock Request', $rejectedStockRequestCount)
+                ->description('Rejected')
+                ->descriptionIcon('heroicon-m-x-circle')
+                ->color('danger')
+                ->url(
+                    route('filament.dashboard.resources.office-stationery-stock-requests.index')
+                )
+                ->icon('heroicon-o-document-text'),
+            
+            Stat::make('Stock Request', $completedStockRequestCount)
                 ->description('Fully processed requests')
                 ->descriptionIcon('heroicon-m-check-badge')
                 ->color('success')
                 ->url(
                     route('filament.dashboard.resources.office-stationery-stock-requests.index', [
                         'tableFilters[status][value]' => OfficeStationeryStockRequest::STATUS_COMPLETED
+                    ])
+                )
+                ->icon('heroicon-o-document-text'),
+
+            // Stock Usages
+            Stat::make('Stock Usage', $pendingStockUsageCount)
+                ->description('Pending approval')
+                ->descriptionIcon('heroicon-m-clock')
+                ->color('primary')
+                ->url(
+                    route('filament.dashboard.resources.office-stationery-stock-usages.index', [
+                        'tableFilters[status][value]' => OfficeStationeryStockUsage::STATUS_PENDING
+                    ])
+                )
+                ->icon('heroicon-o-document-text'),
+
+            Stat::make('Stock Usage', $inProgressStockUsageCount)
+                ->description('In progress')
+                ->descriptionIcon('heroicon-m-clock')
+                ->color('warning')
+                ->url(
+                    route('filament.dashboard.resources.office-stationery-stock-usages.index')
+                )
+                ->icon('heroicon-o-document-text'),
+            
+            Stat::make('Stock Usage', $rejectedStockUsageCount)
+                ->description('Rejected')
+                ->descriptionIcon('heroicon-m-x-circle')
+                ->color('danger')
+                ->url(
+                    route('filament.dashboard.resources.office-stationery-stock-usages.index')
+                )
+                ->icon('heroicon-o-document-text'),
+            
+            Stat::make('Stock Usage', $completedStockUsageCount)
+                ->description('Fully processed usages')
+                ->descriptionIcon('heroicon-m-check-badge')
+                ->color('success')
+                ->url(
+                    route('filament.dashboard.resources.office-stationery-stock-usages.index', [
+                        'tableFilters[status][value]' => OfficeStationeryStockUsage::STATUS_COMPLETED
                     ])
                 )
                 ->icon('heroicon-o-document-text'),
