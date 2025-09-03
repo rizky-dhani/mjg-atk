9cb73aa0a3cbbfaeef60b53438d5b45b1c7b70fc
Rizky Dhani <rizkydhni03@gmail.com>
2025-09-03 09:20:06 +0700

    added RelationManager for MarketingMediaCategory

---
+++ app/Filament/Resources/MarketingMediaCategoryResource/RelationManagers/MarketingMediaItemsRelationManager.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\MarketingMediaCategoryResource\RelationManagers;
+
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class MarketingMediaItemsRelationManager extends RelationManager
+{
+    protected static string $relationship = 'items';
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->recordTitleAttribute('name')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('unit_of_measure')
+                    ->label('Unit'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
