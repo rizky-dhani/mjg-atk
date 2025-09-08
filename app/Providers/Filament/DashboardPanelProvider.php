ad2dc6c962ff2066b63e6996a70531c4927c2853
Rizky Dhani <rizkydhni03@gmail.com>
2025-09-08 16:28:01 +0700

    feat(filament): add filament configuration file and adjust user menu item alignment
    - Introduce `config/filament.php` with comprehensive configuration options for broadcasting, filesystem disk, assets path, cache path, Livewire loading delay, and system route prefix
    - Adjust alignment of user menu items in `DashboardPanelProvider` for better code readability
    - This commit adds essential configuration capabilities for Filament while maintaining existing functionality

--- app/Providers/Filament/DashboardPanelProvider.php
+++ app/Providers/Filament/DashboardPanelProvider.php
@@ -37,9 +37,9 @@ class DashboardPanelProvider extends PanelProvider
             ])
             ->userMenuItems([
                 'profile' => MenuItem::make()
-                            ->label(fn() => auth()->user()->name)
-                            ->url(fn() => (string) EditProfile::getUrl())
-                            ->icon('heroicon-o-user')
+                    ->label(fn() => auth()->user()->name)
+                    ->url(fn() => (string) EditProfile::getUrl())
+                    ->icon('heroicon-o-user')
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
